3c00ee4d4300ace3ec482005512c990292f91f9f
OuryB <jeremie.bartak@gmail.com>
2017-10-17 15:44:36 +0200

    Modification fonction de reconnaissance

--- matlab/Face_detection_haar.m
+++ matlab/Face_detection_haar.m
@@ -6,7 +6,7 @@ I_gris=rgb2gray(I);
 
 [a b]=size(I(:,:,1));
 
-detector = vision.CascadeObjectDetector('FrontalFaceLBP', 'MinSize', [floor(a/10) floor(b/10)]);
+detector = vision.CascadeObjectDetector('FrontalFaceCART', 'MinSize', [floor(a/10) floor(b/10)]);
 
 bboxes=step(detector, I_gris);
 [m,n]=size(bboxes);
