a5ded09a75272c3c9a89a35eef4c1f8ec850c0f9
OuryB <jeremie.bartak@gmail.com>
2017-10-17 14:47:47 +0200

    Ajout des scripts de reconnaissance faciale

---
+++ matlab/Face_detection_haar.m
@@ -0,0 +1,17 @@
+I=imread(input('...'));
+
+%I=imrotate(I,90);
+
+I_gris=rgb2gray(I);
+
+detector = vision.CascadeObjectDetector('FrontalFaceCART','MinSize', [100 100]);
+
+bboxes=step(detector, I_gris);
+[m,n]=size(bboxes);
+
+figure
+imshow(I)
+%hold on
+for i=1:m
+    rectangle('Position',bboxes(i,:),'EdgeColor','r','LineWidth',3)
+end
